4b641af10df618af403dbb6bca3bc149dfd187d7
Alexandr150983 <alexandr150983@gmail.com>
2023-07-14 16:08:43 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach(ingredient => {
+  const li = document.createElement('li');
+
+  li.textContent = ingredient;
+  li.classList.add('item');
+  fragment.appendChild(li);
+  
+});
+list.appendChild(fragment);
+
