78a0f2e1521ede93c00206a37b1442895437a72d
Alexandr150983 <alexandr150983@gmail.com>
2023-07-15 13:40:01 +0300

    task6

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const validationInput = document.getElementById("validation-input");
+
+validationInput.addEventListener('blur', () => {
+    const inputLength = validationInput.value.length;
+    const dataLength = validationInput.getAttribute('data-length');
+
+    validationInput.classList.remove('valid', 'invalid');
+
+    inputLength === Number(dataLength) ? validationInput.classList.add('valid') : validationInput.classList.add('invalid');
+
+});
+
+
