8be299e1c08c31334cba9c743ceabe060530db97
Alexandr150983 <alexandr150983@gmail.com>
2023-07-15 17:10:41 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', el => {
+    el.preventDefault();
+
+    const emailInput = loginForm.elements.email;
+    const passwodrInput = loginForm.elements.password;
+    const formData = {
+        email: emailInput.value,
+        password: passwodrInput.value,
+    }
+
+    if (emailInput.value === '' || passwodrInput.value === '') {
+        alert("Усі поля повинні бути заповнені!");
+    } else {
+        formData;
+
+        console.log(formData);
+
+        loginForm.reset();
+    
+    };
+
+});
