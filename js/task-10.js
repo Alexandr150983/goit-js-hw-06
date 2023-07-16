af870277bb9085f2c57a9eba92024b6ea178fb2b
Alexandr150983 <alexandr150983@gmail.com>
2023-07-16 12:45:42 +0300

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  let boxSize = 30;
+  const boxArray = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxArray.push(box);
+    
+    boxSize += 10;
+  }
+  boxesContainer.append(...boxArray);
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+
+createButton.addEventListener("click", () => {
+  const input = document.querySelector('input[type="number"]');
+  const amount = parseInt(input.value);
+  
+  if (!isNaN(amount) && amount > 0) {
+    createBoxes(amount);
+  } else {
+    alert("Введіть будь-ласка допустиме число більше нуля!");
+  }
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
